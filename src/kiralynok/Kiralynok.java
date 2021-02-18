03ec377895eb680ab3af83ffabc1a7e547840fa6
NadasyB <nadasy.bendeguz@gmail.com>
2021-02-18 10:44:48 +0100

    9. feladat

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -7,7 +7,7 @@ public class Kiralynok {
     private Tabla tabla;
 
     public Kiralynok() {
-        Tabla tabla = new Tabla('#');
+        tabla = new Tabla('#');
     }
     
     
@@ -21,12 +21,12 @@ public class Kiralynok {
     public void megoldasok() {
         feladat4();
         feladat6();
+        feladat9();
     }
 
     private void feladat4() {
         //4. feladat
         System.out.println("4. feladat: üres tábla megjelenítése");
-        Tabla tabla = new Tabla('#');
         tabla.megjelenit();
     }
 
@@ -35,6 +35,13 @@ public class Kiralynok {
         System.out.println("6. feladat: feltöltött tábla");
         tabla.elhelyez(8);
         tabla.megjelenit();
+    }
+
+    private void feladat9() {
+        //9. feladat
+        System.out.println("9. feladat: üres sorok és oszlopok száma");
+        System.out.println("sorok: " + tabla.uresSorokSzama());
+        System.out.println("oszlopok: " + tabla.uresOszlopokSzama());
     }
     
 }
